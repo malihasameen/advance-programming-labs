d0a85c8dded752955c6498a7bbe20b1cb0d1c098
Maliha Sameen <malihasameen58@gmail.com>
2018-10-12 09:35:43 -0700

    added interface

---
+++ Lab4_Strategy_Pattern/EncryptionStrategy.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encryptortest;
+
+/**
+ *
+ * @author msameen.bscs16seecs
+ */
+public interface EncryptionStrategy {
+    void encryptData(String plainText);
+}
